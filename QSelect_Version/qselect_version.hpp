7ca93530c415e59a1daeb584be9cd9c77d04c861
surfactants <8463818+surfactants@users.noreply.github.com>
2025-07-09 23:56:33 -0400

    Update qselect_version.hpp
    style pass

--- QSelect_Version/qselect_version.hpp
+++ QSelect_Version/qselect_version.hpp
@@ -7,10 +7,12 @@ class QSelect_Version : public QWidget
     Q_OBJECT
 
 public:
-    QSelect_Version(QWidget *parent = nullptr, QString ndelimiter = ".", QString nprefix = "v");
+    QSelect_Version(QWidget *parent = nullptr,
+                    const QString ndelimiter = ".",
+                    QString nprefix = "v");
     ~QSelect_Version();
 
-    void setDelimiter(QString ndelimiter);
+    void setDelimiter(const QString& ndelimiter);
     void setPrefix(QString nprefix);
 
     void load(int maj, int min, int pat);
@@ -26,13 +28,13 @@ private:
     QString prefix;
     QString delimiter;
 
-    QLineEdit* major{ nullptr };
-    QLineEdit* minor{ nullptr };
-    QLineEdit* patch{ nullptr };
+    QLineEdit* major { nullptr };
+    QLineEdit* minor { nullptr };
+    QLineEdit* patch { nullptr };
 
     QLabel* sep_0{ nullptr };
     QLabel* sep_1{ nullptr };
     QLabel* sep_2{ nullptr };
 
-    QHBoxLayout* layout{ nullptr };
+    QHBoxLayout* layout { nullptr };
 };
